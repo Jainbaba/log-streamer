8bffbb210ee9738db0e319754e7015c3180dc6ec
Jainbaba <hrithikjain007.hj@gmail.com>
2024-11-07 22:29:20 +0530

    updated docker

--- src/logger/settings.py
+++ src/logger/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = [".railway.app"]
+ALLOWED_HOSTS = [".railway.app","0.0.0.0"]
 
 if DEBUG:
-    ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
+    ALLOWED_HOSTS += ["127.0.0.1", "localhost" , "0.0.0.0"]
 # Application definition
 
 INSTALLED_APPS = [
